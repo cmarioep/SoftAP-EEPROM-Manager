f6501fa6f6a74ef02ddca3119482cdbc3a35855d
cmarioep <cmescandonpardo@gmail.com>
2022-08-15 09:56:39 -0500

    Init: A simple SofAP web server to manage custom environment variables on EEPORM memory

---
+++ SoftAP-EEPROM-Manager.ino
@@ -0,0 +1,13 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
