c038b49abb4f88ab55e4c8234de4aaa7c2b2b30a
cmarioep <cmescandonpardo@gmail.com>
2022-12-10 16:29:16 -0500

    Feature: AppRouter

--- Front end/src/main.jsx
+++ Front end/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import './index.css'
+import { AppRouter } from './router/AppRouter'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   </React.StrictMode>
 )
---
+++ Front end/src/router/AppRouter.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { Home, WifiConfig } from './pages/';
+
+
+export const AppRouter = () => {
+
+    return (
+
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/wificonfig" element={<WifiConfig />} />
+        </Routes>
+
+    )
+
+}
