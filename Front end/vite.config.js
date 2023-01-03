f963fede789f3374c2422dc65abecc191fada138
cmarioep <cmescandonpardo@gmail.com>
2023-01-02 23:21:56 -0500

    Feature: build output config - dir and file names

--- Front end/vite.config.js
+++ Front end/vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+
+  build: {
+    rollupOptions: {
+      output: {
+        dir: '../Firmware/SoftAP-SPIFFS-Manager/data',
+        assetFileNames: 'styles.css',
+        entryFileNames: 'main.js',
+      }
+    },
+
+  }
+  
 })
